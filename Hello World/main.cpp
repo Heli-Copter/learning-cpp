410a1d6249fe9a0bd54c02fdd0cdd37a3820dcc1
Gaurav Sharma <gaurav.min.js@gmail.com>
2017-10-15 15:44:59 +0530

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Gaurav Sharma on 15/10/17.
+//  Copyright © 2017 Gaurav Sharma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
